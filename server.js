b41c82208cb2285f6b729800e6684b7df2f2119d
Daniel Smith <dsmith@digitalmint.io>
2024-07-23 15:23:36 -0500

    updated project to node v18 and yarn

--- server.js
+++ server.js
@@ -1,44 +1,62 @@
-var config      = require('config'),
-    restify     = require('restify'),
-    api         = require('./routes/api'),
-    fs          = require('fs')
+var config = require('config');
+var restify = require('restify');
+var api = require('./routes/api');
+var fs = require('fs');
 
-var app = restify.createServer()
+var app = restify.createServer();
 
-app.use(restify.queryParser())
-app.use(restify.CORS())
-app.use(restify.fullResponse())
+app.use(restify.plugins.queryParser());
+app.use(restify.plugins.fullResponse());
+
+const corsMiddleware = require('restify-cors-middleware');
+const cors = corsMiddleware({
+  origins: ['*'],
+  allowHeaders: ['Authorization'],
+  exposeHeaders: ['Authorization']
+});
+app.pre(cors.preflight);
+app.use(cors.actual);
 
 // Routes
-app.get('/', function (req, res, next)
-  {
-    var data = fs.readFileSync(__dirname + '/index.html');
+app.get('/', function (req, res, next) {
+  var data = fs.readFileSync(__dirname + '/index.html');
 
-    res.status(200);
-    res.header('Content-Type', 'text/html');
-    res.end(data.toString().replace(/host:port/g, req.header('Host')));
-  });
+  res.setHeader('Content-Type', 'text/html');
+  res.send(200, data.toString().replace(/host:port/g, req.header('Host')));
+  return next();
+});
 
-app.get('/v0/q', api.v0.query.get)
-app.get('/v0/s', api.v0.search.get)
-app.get('api/v0/q', api.v0.query.get)
-app.get('api/v0/s', api.v0.search.get)
+app.get('/v0/q', (req, res, next) => {
+  api.v0.query.get(req, res, next);
+});
 
-app.get('/.well-known/status', function (req, res, next)
-  {
-    var codes_2012 = require(process.cwd() + '/data/codes-2012'),
-        missing_zoos = (codes_2012['712130'] == undefined);
-    
-    var status = {
-            'status': (missing_zoos ? 'Missing data' : 'ok'),
-            'updated': Math.floor((new Date()).getTime() / 1000),
-            'dependencies': null,
-            'resources': null,
-        };
-    
-    res.send(status);
-  });
+app.get('/v0/s', (req, res, next) => {
+  api.v0.search.get(req, res, next);
+});
+
+app.get('/api/v0/q', (req, res, next) => {
+  api.v0.query.get(req, res, next);
+});
+
+app.get('/api/v0/s', (req, res, next) => {
+  api.v0.search.get(req, res, next);
+});
+
+app.get('/.well-known/status', function (req, res, next) {
+  var codes_2012 = require(process.cwd() + '/data/codes-2012');
+  var missing_zoos = (codes_2012['712130'] === undefined);
+
+  var status = {
+    'status': (missing_zoos ? 'Missing data' : 'ok'),
+    'updated': Math.floor((new Date()).getTime() / 1000),
+    'dependencies': null,
+    'resources': null,
+  };
+
+  res.send(status);
+  return next();
+});
 
 app.listen(config.port, config.ip, function () {
-  console.log( "Listening on port " + config.port )
-})
+  console.log("Listening on port " + config.port);
+});
